9cc0432a02787c4cdacf3414497610b7a71561bd
madhesh-maasi <madheshmaasi3@gmail.com>
2023-01-31 19:53:25 +0530

    Maasi 31/01/2023

--- src/webparts/customCalendar/components/ICS.tsx
+++ src/webparts/customCalendar/components/ICS.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
 import ICalendarLink from "react-icalendar-link";
-
+import "./ICS.scss";
+import { Icon } from "@fluentui/react";
 export default function ICS(props) {
   const event = {
     title: props.eventdata.EventTitle,
     description: props.eventdata.EventTitle,
     startTime: props.eventdata.EventStart,
     endTime: props.eventdata.EventEnd,
-    location: props.eventdata.location
+    location: props.eventdata.location,
   };
 
   return (
     <div className="App">
+      <Icon iconName="Add" style={{ marginRight: 6 }} />
       <ICalendarLink event={event}>Add to Calendar</ICalendarLink>
     </div>
   );
