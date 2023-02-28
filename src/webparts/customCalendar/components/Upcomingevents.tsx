d9fa37840ab3a3996e07dccd1306aa4f8c4585f4
kalimuthu949 <kalimuthu210@gmail.com>
2023-02-28 17:40:23 +0530

    after permission changes by kali

--- src/webparts/customCalendar/components/Upcomingevents.tsx
+++ src/webparts/customCalendar/components/Upcomingevents.tsx
@@ -20,10 +20,10 @@ const Upcomingevents = (props) => {
     let enddate = moment().format("YYYY-MM-DD");
     if (props.pivot == 1) {
       startdate = moment().format("YYYY-MM-DD");
-      enddate = moment().add(30, "days").format("YYYY-MM-DD"); //one month data from today
+      enddate = moment().add(30, "days").format("YYYY-MM-DD"); //three month data from today
     } else if (props.pivot == 2) {
       enddate = moment().format("YYYY-MM-DD");
-      startdate = moment().subtract(90, "days").format("YYYY-MM-DD"); //three month before data from today.
+      startdate = moment().subtract(180, "days").format("YYYY-MM-DD"); //six month before data from today.
     }
     await graph.groups
       .getById(props.groupID)
